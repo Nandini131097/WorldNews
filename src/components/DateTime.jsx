6ee6f0f1cc49195bcb3def2e4f787880940b9e77
Nandini Lamghare <lamgharenandini@gmail.com>
2024-08-31 00:57:39 +0530

    hamburger created

--- src/components/DateTime.jsx
+++ src/components/DateTime.jsx
@@ -7,15 +7,54 @@ function DateTime() {
         const dateAndTime = new Date();
         document.getElementById("currentFullDate").innerHTML = dateAndTime.toString();
 
+       
+
     }, [])
 
+    
+    const ClickHamburger=()=>{
+      const navContainerHamElement = document.getElementById('navContainerHam');
+      if(navContainerHamElement.style.display==='none'){
+        navContainerHamElement.style.display='block';
+      }
+      else{
+        navContainerHamElement.style.display='none';
+      }
+    }
+    
+
+
   
-
-
   return (
+    <>
     <div id='DateTime'>
         <p id='currentFullDate'></p>
+        <button onClick={ClickHamburger} className='hamburger'>
+          <i className="fa-duotone fa-solid fa-bars"></i>
+        </button>
     </div>
+
+    <div id='navContainerHam'>
+    <button onClick={ClickHamburger} id='crossHambar'>
+        <i className="fa-solid fa-xmark"></i>
+        </button>
+          <ul >
+                  <li><a href="#home">Home</a></li>
+                  <li><a href="#about">About</a></li>
+                  <li><a href="#services">Categories</a>
+                  <ul id='CategoryList'>
+                    <li id="CatList1">Business</li>
+                    <li id="CatList2">Sports</li>
+                    <li id="CatList3">Politices</li>
+                    <li id="CatList4">LifeStyle</li>
+                  </ul>
+                  </li>
+                  <li><a href="#contact">Contact</a></li>
+              </ul>
+        
+    </div>
+    </>
+    
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <nav className='Nav-Bar'>
+    <nav id='Nav-Bar'>
        <ul className="nav-links">
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
